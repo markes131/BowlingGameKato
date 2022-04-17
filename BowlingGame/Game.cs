8cac3cc3c22c5a724cd550bc12eeaf4c1cbdcd68
Piotr Markowski <markowskipriv@gmail.com>
2022-04-17 20:15:17 +0200

    Created base project for BowlingGame with few basic methods via TDD

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        public int GameScore { get; set; }
+
+        public int Pins { get; set; }
+
+
+        public Game()
+        {
+            GameScore = 0;
+        }
+        
+        public int Score()
+        {
+            return GameScore;
+        }
+
+        public int Throw(int rolledPins)
+        {
+            Pins = rolledPins;
+            GameScore += Pins;
+
+            return Pins;
+        }
+
+        public int ManyThrows(int numberOfThrows)
+        {
+            int sum = 0;
+            for (int i=0; i < numberOfThrows; i++)
+            {
+                sum += Throw(1);
+            }
+
+            return sum;
+        }
+
+    }
+
+}
